0fd4c1ef945a103e2a47dd98bcc5f5518ad178e0
Andy Amaya <andy@Andys-MacBook-Pro-2.local>
2020-03-28 14:01:11 -0700

    Remove AppLoading component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,9 @@ import AppStyles from './AppStyles';
 import MenuPanel from './MenuPanel';
 import TopNavBar from './TopNavBar';
 import TopNotifications from './TopNotifications';
-import AppLoading from './AppLoading';
 
 export {
   AppStyles,
-  AppLoading,
   MenuPanel,
   TopNavBar,
   TopNotifications,
