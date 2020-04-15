7cfc8cb8d94e3751dcacafcf99a3891721d01c89
Andy Amaya <aamay001@gmail.com>
2020-04-15 15:50:15 -0700

    Update wide ad.

--- src/components/General/WideAd.jsx
+++ src/components/General/WideAd.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const WideAd = () => (
   <div id="659040877">
-    <script type="text/javascript">
-      {`try {
+    {`<script type="text/javascript">
+      try {
           window._mNHandle.queue.push(function () {
             window._mNDetails.loadTag("659040877", "970x250", "659040877");
           });
         } catch (error) {}
-      `}
-    </script>
+    </script>`}
   </div>
 );
 
