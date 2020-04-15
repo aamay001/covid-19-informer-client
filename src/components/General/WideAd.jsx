165559d669dd2109c39ba8e0320a70cd5a50c4c0
Andy Amaya <aamay001@gmail.com>
2020-04-15 15:57:40 -0700

    Update Widead.

--- src/components/General/WideAd.jsx
+++ src/components/General/WideAd.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+/* globals document */
+import React, { Component } from 'react';
 
-const WideAd = () => (
-  <div id="659040877">
-    {`<script type="text/javascript">
-      try {
-          window._mNHandle.queue.push(function () {
-            window._mNDetails.loadTag("659040877", "970x250", "659040877");
-          });
-        } catch (error) {}
-    </script>`}
-  </div>
-);
+class WideAd extends Component {
+  componentDidMount() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.innerHTML = `try {
+      window._mNHandle.queue.push(function () {
+        window._mNDetails.loadTag("659040877", "970x250", "659040877");
+      });
+    } catch (error) {
+      console.log(error);
+    }`;
+    this.parentDiv.appendChild(s);
+  }
+
+  render() {
+    return (
+      <div id="659040877" ref={(el) => { this.parentDiv = el; }} />
+    );
+  }
+}
 
 export default WideAd;
