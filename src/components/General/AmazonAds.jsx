472745da6efd4c0433f97fd0efb441b0b98d537f
Andy Amaya <aamay001@gmail.com>
2020-04-16 07:58:26 -0700

    Better AmzAds.

--- src/components/General/AmazonAds.jsx
+++ src/components/General/AmazonAds.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+/* globals document */
+import React, { Component } from 'react';
 
-const AmazonAds = () => (
-  <>
-    <div id="amzn-assoc-ad-03b97141-dc44-4401-9f6d-7745e1682edb" />
-    <script
-      async
-      src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=03b97141-dc44-4401-9f6d-7745e1682edb"
-    />
-  </>
-);
+class AmazonAds extends Component {
+  componentDidMount() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=03b97141-dc44-4401-9f6d-7745e1682edb';
+    this.parentDiv.appendChild(s);
+  }
+
+  render() {
+    return (
+      <div ref={(el) => { this.parentDiv = el; }}>
+        <div id="amzn-assoc-ad-03b97141-dc44-4401-9f6d-7745e1682edb" />
+      </div>
+    );
+  }
+}
 
 export default AmazonAds;
